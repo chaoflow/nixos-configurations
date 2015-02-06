69ff6fcae7557079043c1b69d3ad2f0213a3753d
Florian Friesdorf <flo@chaoflow.net>
2015-02-06 18:30:20 +0100

    host network config

--- configuration-eve.nix
+++ configuration-eve.nix
@@ -238,11 +238,8 @@
     interfaceMonitor.enable = false; # Watch for plugged cable.
     # host network for qemu-kvm
     localCommands = ''
-      ${pkgs.vde2}/bin/vde_switch -tap tap0 -mod 660 -group kvm -daemon
-      ip addr add 10.0.0.1/24 dev tap0
-      ip link set dev tap0 up
+      ip addr add 10.0.0.1/24 dev lo
       ${pkgs.procps}/sbin/sysctl -w net.ipv4.ip_forward=1
-      ${pkgs.iptables}/sbin/iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -j MASQUERADE
     '';
     wireless.enable = true;
     wireless.driver = "nl80211";
